b97b92756ed9e152eef1d759fbbc8b6b01cfb886
Thura Lin Htut <thuralinhtut3@gmail.com>
2022-11-06 18:05:20 +0630

    commit

--- salesdigits/auth/Signup.js
+++ salesdigits/auth/Signup.js
@@ -1,12 +1,56 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text,TextInput,ScrollView,Button} from 'react-native';
 
 const SignUp = () => {
+
+  const [username,setUsername] = React.useState()
+  const [password,setPassword] = React.useState()
+  const [phoneno,setPhoneno] = React.useState()
+  const [name,setName] = React.useState()
+
+
+
   return (
-    <View>
-      <Text>What I am </Text>
-    </View>
+   <ScrollView>
+      <View style={{padding: 10}}>
+        <Text style={{color: 'black'}}>Username</Text>
+        <TextInput
+          style={styles.textinput}
+          placeholder={'Username'}
+
+          // onTextChange={e => (username.current = e)}
+        />
+        <Text style={{color: 'black'}}>Name</Text>
+        <TextInput
+          style={styles.textinput} 
+          placeholder={'Shop Name Or Other'}
+          // onTextChange={e => (username.current = e)}
+        />
+        <Text style={{color: 'black'}}>Name</Text>
+        <TextInput
+          style={styles.textinput}
+          placeholder={'Shop Name Or Other'}
+
+          // onTextChange={e => (username.current = e)}
+        />
+        <Text style={{color: 'black'}}>Password</Text>
+        <TextInput
+          style={styles.textinput}
+          placeholder={'Password'}
+          secureTextEntry={visible}
+          // onTextChange={e => (password.current = e)}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color: 'white'}}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Singup')}>
+          <Text style={{color: 'white'}}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
--- salesdigits/server/data.js
+++ salesdigits/server/data.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
 
+
+class Database (){
+	login({queryKey}){
+		const [_,username,password] = queryKey;
+
+		return axios.post("/auth/login/",{username:username,password});
+	}
+
+	regiser({queryKey}){
+		const [_,username,password,name,phoneno] = queryKey;
+
+		return axois.post("auth/salesdigit/register/",{username:username,name:name,phoneno:phoneno,password:password});
+	}
+
+
+
+}	
